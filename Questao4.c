3f9a6cd74f7fb5e8bcc1c4f3bb8f34eabbdc4ce6
1LGSantos <lucasgrasselicontato@gmail.com>
2026-08-25 21:38:59 -0300

    Add files via upload

---
+++ Questao4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 1000
+
+// Inverte a string s (iterativo) e escreve o resultado em resp
+void inverterString(char s[], char resp[]) {
+    int tamanho = 0;
+    while (s[tamanho] != '\0') {
+        tamanho++;
+    }
+    int i;
+    for (i = 0; i < tamanho; i++) {
+        resp[i] = s[tamanho - 1 - i];
+    }
+    resp[tamanho] = '\0';
+}
+
+int main(void) {
+    char linha[TAM];
+    char invertida[TAM];
+    while (fgets(linha, TAM, stdin) != NULL) {
+        int len = 0;
+        while (linha[len] != '\0') {
+            len++;
+        }
+        if (len > 0 && linha[len - 1] == '\n') {
+            linha[len - 1] = '\0';
+        }
+        inverterString(linha, invertida);
+        printf("%s\n", invertida);
+    }
+    return 0;
+}
